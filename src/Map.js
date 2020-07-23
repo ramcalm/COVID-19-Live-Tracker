0ed2c493e2586ae60b2d0174ca99c056e40d1a2f
ramcalm <ram.callm@gmail.com>
2020-07-24 00:15:40 +0530

    Map added

--- src/Map.js
+++ src/Map.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import "./Map.css"
-import {Map as LeafletMap, TileLayer } from 'react-leaflet';
+import React from "react";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
+import "./Map.css";
+import { showDataOnMap } from "./util";
 
+const Map = ({ center, zoom, countries, casesType }) => {
+  return (
+    <div className="map">
+      <LeafletMap center={center} zoom={zoom}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {showDataOnMap(countries, casesType)}
+      </LeafletMap>
+    </div>
+  );
+};
 
-function Map({ countries, center, zoom }) {
-    return (
-        <div className="map">
-            <LeafletMap center={center} zoom={zoom}>
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                />
-            </LeafletMap>   
-            {/* Loop through and draw circles */}        
-        </div>
-    )
-}
-
-export default Map
+export default Map;
